adaa471f09884c96087032acc00f5f4ea295deac
deepak-1dee <baldeyadav747286@gmail.com>
2025-11-09 17:25:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleCode(id) {
+  const code = document.getElementById(id);
+  if (code.style.display === "block") {
+    code.style.display = "none";
+  } else {
+    code.style.display = "block";
+  }
+}
