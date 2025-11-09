7284a1f724509abfcdd9eb61a4ba7caf5a2ff47a
deepak-1dee <baldeyadav747286@gmail.com>
2025-11-09 17:23:31 +0530

    Delete deepak c project directory

--- deepak c project/script.js
+++
@@ -1,8 +0,0 @@
-function toggleCode(id) {
-  const code = document.getElementById(id);
-  if (code.style.display === "block") {
-    code.style.display = "none";
-  } else {
-    code.style.display = "block";
-  }
-}
